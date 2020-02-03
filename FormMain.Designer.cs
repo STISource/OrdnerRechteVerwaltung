82fcef12430a7357b9ebc0945d203e56c15d8050
Jörg Ahlrep <edv5@sti-bearings.com>
2020-02-03 13:24:56 +0100

    Projektdateien hinzufügen.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace OrdnerRechteVerwaltung
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtErr = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.edBasis = new System.Windows.Forms.TextBox();
+            this.edMaske = new System.Windows.Forms.TextBox();
+            this.butSearch = new System.Windows.Forms.Button();
+            this.lbCounter = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.edUser = new System.Windows.Forms.TextBox();
+            this.grdPfade = new System.Windows.Forms.DataGridView();
+            this.butDel = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPfade)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtErr
+            // 
+            this.txtErr.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtErr.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtErr.ForeColor = System.Drawing.Color.Red;
+            this.txtErr.Location = new System.Drawing.Point(15, 349);
+            this.txtErr.Multiline = true;
+            this.txtErr.Name = "txtErr";
+            this.txtErr.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtErr.Size = new System.Drawing.Size(655, 39);
+            this.txtErr.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Basis-Pfad";
+            // 
+            // edBasis
+            // 
+            this.edBasis.Location = new System.Drawing.Point(90, 6);
+            this.edBasis.Name = "edBasis";
+            this.edBasis.Size = new System.Drawing.Size(190, 20);
+            this.edBasis.TabIndex = 2;
+            this.edBasis.Text = "K:\\11 Projekte";
+            // 
+            // edMaske
+            // 
+            this.edMaske.Location = new System.Drawing.Point(374, 7);
+            this.edMaske.Name = "edMaske";
+            this.edMaske.Size = new System.Drawing.Size(130, 20);
+            this.edMaske.TabIndex = 3;
+            this.edMaske.Text = "Kunde";
+            // 
+            // butSearch
+            // 
+            this.butSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butSearch.Location = new System.Drawing.Point(304, 28);
+            this.butSearch.Name = "butSearch";
+            this.butSearch.Size = new System.Drawing.Size(200, 23);
+            this.butSearch.TabIndex = 4;
+            this.butSearch.Text = "Verzeichnisse suchen";
+            this.butSearch.UseVisualStyleBackColor = true;
+            this.butSearch.Click += new System.EventHandler(this.butSearch_Click);
+            // 
+            // lbCounter
+            // 
+            this.lbCounter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbCounter.AutoSize = true;
+            this.lbCounter.Location = new System.Drawing.Point(645, 37);
+            this.lbCounter.Name = "lbCounter";
+            this.lbCounter.Size = new System.Drawing.Size(25, 13);
+            this.lbCounter.TabIndex = 5;
+            this.lbCounter.Text = "000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(301, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Such-Pfad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "User / Gruppe";
+            // 
+            // edUser
+            // 
+            this.edUser.Location = new System.Drawing.Point(90, 30);
+            this.edUser.Name = "edUser";
+            this.edUser.Size = new System.Drawing.Size(190, 20);
+            this.edUser.TabIndex = 9;
+            this.edUser.Text = "STI\\Einkauf_reduziert";
+            // 
+            // grdPfade
+            // 
+            this.grdPfade.AllowUserToAddRows = false;
+            this.grdPfade.AllowUserToDeleteRows = false;
+            this.grdPfade.AllowUserToOrderColumns = true;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.grdPfade.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.grdPfade.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grdPfade.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.grdPfade.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.grdPfade.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.grdPfade.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdPfade.Location = new System.Drawing.Point(15, 60);
+            this.grdPfade.Name = "grdPfade";
+            this.grdPfade.ReadOnly = true;
+            this.grdPfade.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdPfade.Size = new System.Drawing.Size(655, 287);
+            this.grdPfade.TabIndex = 10;
+            // 
+            // butDel
+            // 
+            this.butDel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.butDel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butDel.ForeColor = System.Drawing.Color.Red;
+            this.butDel.Location = new System.Drawing.Point(265, 420);
+            this.butDel.Name = "butDel";
+            this.butDel.Size = new System.Drawing.Size(405, 23);
+            this.butDel.TabIndex = 12;
+            this.butDel.Text = "Zugriff auf alle oben aufgeführten Verzeichnisse verweigern";
+            this.butDel.UseVisualStyleBackColor = true;
+            this.butDel.Click += new System.EventHandler(this.butDel_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 397);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(442, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Zum Ändern von Berechtigungen muss man als Administrtor angemeldet sein!";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.butDel);
+            this.Controls.Add(this.grdPfade);
+            this.Controls.Add(this.edUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbCounter);
+            this.Controls.Add(this.butSearch);
+            this.Controls.Add(this.edMaske);
+            this.Controls.Add(this.edBasis);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtErr);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Ordner-Rechte Verwaltung";
+            ((System.ComponentModel.ISupportInitialize)(this.grdPfade)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtErr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox edBasis;
+        private System.Windows.Forms.TextBox edMaske;
+        private System.Windows.Forms.Button butSearch;
+        private System.Windows.Forms.Label lbCounter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox edUser;
+        private System.Windows.Forms.DataGridView grdPfade;
+        private System.Windows.Forms.Button butDel;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Principal;
+using System.Security.AccessControl;
+
+namespace OrdnerRechteVerwaltung
+{
+    public partial class frmMain : Form
+    {
+        public class Berechtigung
+        {
+            public string Pfad { get; set; }
+            public bool berechtigt { get; set; }
+        }
+
+        public BindingList<Berechtigung> Berechtigungen = new BindingList<Berechtigung>();
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        public static Boolean ReadFolderPermission(DirectoryInfo di, string AccountName)
+        {
+            Boolean ergebnis;
+            ergebnis = false;
+
+            //DirectoryInfo di = new DirectoryInfo(path);
+            DirectorySecurity acl = di.GetAccessControl(AccessControlSections.All);
+            AuthorizationRuleCollection rules = acl.GetAccessRules(true, true, typeof(NTAccount));
+
+            //Go through the rules returned from the DirectorySecurity
+            foreach (AuthorizationRule rule in rules)
+            {
+                //If we find one that matches the identity we are looking for
+                if (rule.IdentityReference.Value.Equals(AccountName, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    var filesystemAccessRule = (FileSystemAccessRule)rule;
+
+                    //Cast to a FileSystemAccessRule to check for access rights
+                    if ((filesystemAccessRule.FileSystemRights & FileSystemRights.WriteData) > 0 && filesystemAccessRule.AccessControlType != AccessControlType.Deny)
+                    {
+                        ergebnis = true;
+                    }
+                    else
+                    {
+                        ergebnis = false;
+                    }
+                }
+
+
+            }
+            return ergebnis;
+        }
+
+
+        public static void DenyFolderPermission(string folderPath, string userGrp)
+        {
+            var directoryInfo = new DirectoryInfo(folderPath);
+            var directorySecurity = directoryInfo.GetAccessControl();
+
+            var fileSystemRule = new FileSystemAccessRule(userGrp,
+                                                          FileSystemRights.FullControl,
+                                                          InheritanceFlags.ObjectInherit | InheritanceFlags.ContainerInherit,
+                                                          PropagationFlags.None,
+                                                          AccessControlType.Deny);
+
+            directorySecurity.AddAccessRule(fileSystemRule);
+            directoryInfo.SetAccessControl(directorySecurity);
+        }
+
+
+
+        private void butSearch_Click(object sender, EventArgs e)
+        {
+            Berechtigungen.Clear();
+            grdPfade.Update();
+
+            txtErr.Text = "";
+            txtErr.Update();
+
+            int count = 0;
+            lbCounter.Text = "0";
+            lbCounter.Update();
+
+            Cursor = Cursors.WaitCursor;
+
+            try
+            {
+                grdPfade.DataSource = Berechtigungen;
+
+                // Take a snapshot of the file system.  
+                DirectoryInfo dir = new DirectoryInfo(edBasis.Text);
+
+                // This method assumes that the application has discovery permissions  
+                // for all folders under the specified path.  
+                IEnumerable<DirectoryInfo> dirList = dir.GetDirectories(edMaske.Text, SearchOption.AllDirectories);
+               
+                foreach (DirectoryInfo di in dirList)
+                {
+
+                    //Berechtigungen.Add(new Berechtigung { Pfad = di.FullName, berechtigt = ReadFolderPermission(di, edUser.Text) });
+                    Berechtigungen.Add(new Berechtigung { Pfad = di.FullName, berechtigt = CanReadDirectory(di.FullName, edUser.Text) });
+
+                    count = count + 1;
+                    lbCounter.Text = count.ToString();
+                    lbCounter.Update();
+                }
+                if (count == 0)
+                {
+                    txtErr.Text += "Keine Ordner gefunden die den Suchkriterien entsprechen.";
+                }
+
+
+            }
+            catch (Exception err)
+            {
+                txtErr.Text += err.Message;
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+            }
+
+        }
+
+
+
+        private bool CanReadDirectory(string folder, string user)
+        {
+            bool hasAccess = false;
+            //Step 1. Get the user
+            NTAccount acc = new NTAccount(user);
+            SecurityIdentifier secId = acc.Translate(typeof(SecurityIdentifier)) as SecurityIdentifier;
+
+            DirectorySecurity dirSec = Directory.GetAccessControl(folder);
+
+            //Step 2. Get directory permission details for each user/group
+            AuthorizationRuleCollection authRules = dirSec.GetAccessRules(true, true, typeof(SecurityIdentifier));
+
+            foreach (FileSystemAccessRule ar in authRules)
+            {
+                if (secId.CompareTo(ar.IdentityReference as SecurityIdentifier) == 0)
+                {
+                    var fileSystemRights = ar.FileSystemRights;
+
+                    // *** ERROR: BRÜCKSICHTIGT NICHT DASS DENY ALLE ANDEREN RECHTE ÜBERSTIMMT **************************
+                    
+                    //Step 3. Check file system rights here, read / write as required
+                    if ((fileSystemRights == FileSystemRights.Read ||
+                        fileSystemRights == FileSystemRights.ReadAndExecute ||
+                        fileSystemRights == FileSystemRights.ReadData ||
+                        fileSystemRights == FileSystemRights.ListDirectory) && (ar.AccessControlType != AccessControlType.Deny))
+                    {
+                        hasAccess = true;
+                    }
+
+                    if ((fileSystemRights == (FileSystemRights.ReadAndExecute | FileSystemRights.Synchronize)) && (ar.AccessControlType != AccessControlType.Deny))
+                    {
+                        hasAccess = true;
+                    }
+
+                    // DENY übersteuert alle anderen Rechte
+                    if (ar.AccessControlType == AccessControlType.Deny)
+                    {
+                        hasAccess = false;
+                        break;
+                    }
+                }
+            }
+            return hasAccess;
+        }
+
+        private void butDel_Click(object sender, EventArgs e)
+        {
+        
+            Cursor = Cursors.WaitCursor;
+
+            try
+            {
+                int count = 0;
+                
+                foreach (Berechtigung be in Berechtigungen)
+                {
+                    DenyFolderPermission(be.Pfad, edUser.Text);
+                    
+                    count = count + 1;
+                    lbCounter.Text = count.ToString();
+                    lbCounter.Update();
+                }
+            }
+            catch (Exception err)
+            {
+                txtErr.Text += err.Message;
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+            }
+        }
+    }
+}
